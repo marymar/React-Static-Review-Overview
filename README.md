b9675c1ad66bca8950a05c78d4b7d8d69ba0a1ec
Mary Vokicic <Mary.Vokicic@arvato.com>
2018-01-26 15:50:17 +0100

    aligned header

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
-# Review Overview Example With React-Static
+# React-Static example: Review Overview
 
-To get started, run `react-static create` and use the `basic` template.
+## Development
+
+Start the development server with `npm run start`
+Usually it starts on port 3000: http://localhost:3000/
+
+## Build
+Run `npm run build` to build the project. Static files are generated into dist folder.
+
+---
 
 More info: https://github.com/nozzle/react-static
-
 
 Live Example:
 https://marymar.github.io/react-static-review-overview/
--- src/containers/Header.js
+++ src/containers/Header.js
@@ -5,7 +5,9 @@ import logoImg from '../logo-alodo.png'
 export default getSiteProps(({ name, quote }) => (
   <header className="header">
     <div className="header__content">
-      <img className="header__logo" src={logoImg} alt="Unicorn"/>
+      <div className="header__logo">
+        <img className="rwd-img" src={logoImg} alt="Unicorn" />
+      </div>
       <div className="header__sprintinfo">
         <h1 className="header__headline">Unicorn Review</h1>
         <div className="header__sprintinfo-name">Review: "{name}"</div>
